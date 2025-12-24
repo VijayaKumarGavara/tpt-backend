0e375bd8b1a7e3ae653403b250c35aed7f289a96
VijayaKumarGavara <ggana6947@gmail.com>
2025-12-24 16:07:31 +0530

    changing db query

--- dbQuery.js
+++ dbQuery.js
@@ -2,8 +2,9 @@ import ConnectDB from "./dbConnection.js";
 
 const pool = ConnectDB();
 
-export const dbQuery = (sql, values = []) => {
+export const dbQuery = (sql, values = [],...other) => {
   console.log("SQL:", sql);
   console.log("Values:", values);
+  console.log("Other", other);
   return pool.query(sql, values);
 };
